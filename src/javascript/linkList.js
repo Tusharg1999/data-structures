c4e0cf9016d3c81ecc720ca71d7f2a07131c39e3
tusharg1999 <tusharg1999>
2020-02-29 22:54:22 +0530

    linked list

---
+++ src/javascript/linkList.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    if (this.head == null) {
+      this.head = new Node(data);
+      return;
+    }
+    let current = this.head;
+    while (current.next != null) {
+      current = current.next;
+    }
+    current.next = new Node(data);
+  }
+  addAfterPositon(data,position){
+      const newNode=new Node(data);
+      let current=this.head;
+      for(let i=0;i<position-1;i++){
+        current=current.next;
+      }
+      newNode.next=current.next;
+      current.next=newNode;
+  }
+
+  display() {
+    let current = this.head;
+    let s = "";
+    while (current != null) {
+      s = s + current.data + " -> " + (current.next == null ? "NULL" : "");
+      current = current.next;
+    }
+    console.log(s);
+  }
+}
+
+const list = new LinkedList();
+list.add(3);
+list.add(765);
+list.add(43);
+list.add(423);
+list.add(3);
+list.addAfterPositon(300,2)
+list.display();
